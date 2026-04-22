e12087abd75e39802445cff2a80c0e512c3eb1db
davidwibowo129 <davidwibowo129@gmail.com>
2026-04-22 20:23:54 +0700

    Latihan Variabel dari Modul 8

---
+++ Modul8/latihanVariabel.php
@@ -0,0 +1,18 @@
+<?php
+$hari1 = "Senin";
+$hari2 = "Selasa";
+$hari3 = "Rabu";
+$hari4 = "Kamis";
+$hari5 = "Jumat";
+$hari6 = "Sabtu";
+$hari7 = "Minggu";
+
+echo "Daftar nama hari dalam seminggu:<br>";
+echo "1. " . $hari1 . "<br>";
+echo "2. " . $hari2 . "<br>";
+echo "3. " . $hari3 . "<br>";
+echo "4. " . $hari4 . "<br>";
+echo "5. " . $hari5 . "<br>";
+echo "6. " . $hari6 . "<br>";
+echo "7. " . $hari7 . "<br>";
+?>
