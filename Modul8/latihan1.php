bc116fc3a9e0bb812f3aebf67011413b2a8c3174
davidwibowo129 <davidwibowo129@gmail.com>
2026-04-22 20:23:00 +0700

    Latihan 1 dari Modul 8

---
+++ Modul8/latihan1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Div dan Span</title>
+    <link rel="icon" type="img/png" href="gambar/icon.png" sizes="16x16" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Belajar PHP">
+    <meta name="keywords" content="253307044"> 
+    <meta name="author" content="David Qhoiri Satriyo Wibowo">
+</head>
+<body>
+    <h1>Halaman PHP saya</h1>
+
+    <?php
+        echo "Hallo ini halaman dengan menggunakan bahasa PHP!";
+    ?>
+</body>
+</html>
