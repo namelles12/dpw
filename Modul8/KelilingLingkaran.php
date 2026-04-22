74fc04245ed8dd4a9361716081b64cef6b0bebd7
davidwibowo129 <davidwibowo129@gmail.com>
2026-04-22 20:24:24 +0700

    Latihan Menghitung Keliling Lingkaran dari Modul 8

---
+++ Modul8/KelilingLingkaran.php
@@ -0,0 +1,11 @@
+<?php
+$r = 15; 
+define("PHI", 3.14);
+
+$keliling = 2 * PHI * $r;
+
+echo "<h3>Menghitung Keliling Lingkaran</h3>";
+echo "Jari-jari lingkaran: " . $r . " cm <br>";
+echo "Nilai PHI: " . PHI . "<br>";
+echo "Keliling Lingkaran adalah: " . $keliling . " cm";
+?>
