76bf0a7551e515543aabbd4357e21a7c783f4879
davidwibowo129 <davidwibowo129@gmail.com>
2026-04-27 14:08:39 +0700

    Latihan 2 dari Modul 8

--- Modul8/latihan2.php
+++ Modul8/latihan2.php
@@ -7,9 +7,9 @@
     echo "<p>isi Variable txt adalah: $txt</p>"; 
     echo "<p>isi Variable x adalah: $x</p>"; 
     echo "<p>isi Variable y adalah: $y</p>"; 
-    echo "Belajar PHP di " . $txt2 . "<br>"; // [cite: 48, 49]
+    echo "Belajar PHP di " . $txt2 . "<br>"; 
     echo $x + $y; 
     echo "<br>";
     define("nama_konstanta", "David Qhoiri Satriyo Wibowo"); 
-    echo "<br>" . nama_konstanta; // [cite: 53]
+    echo "<br>" . nama_konstanta; 
 ?>
