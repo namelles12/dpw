0774d79fc0504ae24354eb86448a0a2733dbc072
davidwibowo129 <davidwibowo129@gmail.com>
2026-04-27 14:21:04 +0700

    Latihan Konversi Terbilang dari Modul 8

---
+++ Modul8/konversiTerbilang.php
@@ -0,0 +1,38 @@
+<?php
+$angka = 5; 
+
+echo "Angka: " . $angka . "<br>";
+echo "Terbilang: ";
+switch ($angka) {
+    case 1:
+        echo "satu";
+        break;
+    case 2:
+        echo "dua";
+        break;
+    case 3:
+        echo "tiga";
+        break;
+    case 4:
+        echo "empat";
+        break;
+    case 5:
+        echo "lima";
+        break;
+    case 6:
+        echo "enam";
+        break;
+    case 7:
+        echo "tujuh";
+        break;
+    case 8:
+        echo "delapan";
+        break;
+    case 9:
+        echo "sembilan";
+        break;
+    default:
+        echo "Angka di luar rentang 1-9";
+        break;
+}
+?>
