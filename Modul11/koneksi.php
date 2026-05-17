efb50359859e79b1980c00a7672e54277ceaba36
davidwibowo129 <davidwibowo129@gmail.com>
2026-05-17 18:31:01 +0700

    membuat file koneksi ke database db_praktik menggunakan fungsi mysqli_connect prosedural

---
+++ Modul11/koneksi.php
@@ -0,0 +1,20 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $user = "root";
+    private $paswd = "";
+    private $name = "db_praktik";
+    public $con;
+
+    public function __construct() {
+        $this->con = new mysqli($this->host, $this->user, $this->paswd, $this->name);
+        if ($this->con->connect_error) {
+            die("Connection failed: " . $this->con->connect_error);
+        }
+        $this->con->set_charset("utf8");
+    }
+}
+
+$db = new Database();
+$link = $db->con;
+?>
